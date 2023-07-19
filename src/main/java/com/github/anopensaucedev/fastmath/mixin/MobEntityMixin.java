f9a0cfcf45e7611e227d1224ee20ea23812afd95
AnOpenSauceDev <119765865+AnOpenSauceDev@users.noreply.github.com>
2023-07-19 20:05:51 +1000

    make this into a PR

---
+++ src/main/java/com/github/anopensaucedev/fastmath/mixin/MobEntityMixin.java
@@ -0,0 +1,8 @@
+package com.github.anopensaucedev.fastmath.mixin;
+
+import net.minecraft.entity.mob.MobEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(MobEntity.class)
+public class MobEntityMixin {
+}
