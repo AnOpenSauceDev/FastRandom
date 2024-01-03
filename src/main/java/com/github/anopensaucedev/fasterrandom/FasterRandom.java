e769b2a5b066de2f13892d780f4f123d4ba0536b
Steveplays28 <djspaargaren@outlook.com>
2024-01-03 20:35:05 +0100

    refactor: Improve naming consistency and mix into the random classes themselves

---
+++ src/main/java/com/github/anopensaucedev/fasterrandom/FasterRandom.java
@@ -0,0 +1,19 @@
+package com.github.anopensaucedev.fasterrandom;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FasterRandom implements ModInitializer {
+	public static final String MOD_ID = "faster-random";
+	public static final String MOD_NAME = "Faster Random";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Loading {} \\0_0/", MOD_NAME);
+		LOGGER.warn(
+				"While terrain generation will \"function\" properly, cave generation will have a different seed (in singleplayer / if this mod is on the server), which may or may not be noticeable. Keep this in mind.");
+		LOGGER.warn("If you have any terrain-related issues, make sure to mention me (AnOpenSauceDev) in an issue just to be sure!");
+	}
+}
