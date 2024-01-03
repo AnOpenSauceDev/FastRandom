8104b764dc60a3a8c60368f2b766aaef09d42f68
Steveplays28 <djspaargaren@outlook.com>
2024-01-03 21:15:26 +0100

    feat: Optimise `BaseRandom` and move Gaussian optimisation into `GaussianGenerator`

--- src/main/java/com/github/anopensaucedev/fasterrandom/mixin/GaussianGeneratorMixin.java
+++ src/main/java/com/github/anopensaucedev/fasterrandom/mixin/GaussianGeneratorMixin.java
@@ -1,17 +1,17 @@
 package com.github.anopensaucedev.fasterrandom.mixin;
 
 import com.github.anopensaucedev.fasterrandom.util.FastThreadLocalRandom;
-import net.minecraft.util.math.random.CheckedRandom;
+import net.minecraft.util.math.random.GaussianGenerator;
 import org.jetbrains.annotations.NotNull;
-import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(CheckedRandom.class)
-public class CheckedRandomMixin {
-	@Inject(method = "nextGaussian", at = @At(value = "HEAD"), cancellable = true)
-	private void fastrandom$nextGaussianInject(@NotNull CallbackInfoReturnable<Double> cir) {
+@Mixin(GaussianGenerator.class)
+public class GaussianGeneratorMixin {
+	@Inject(method = "next", at = @At(value = "HEAD"), cancellable = true)
+	private void fastrandom$nextInject(@NotNull CallbackInfoReturnable<Double> cir) {
 		cir.setReturnValue(FastThreadLocalRandom.nextGaussian());
 	}
 }
