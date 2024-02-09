f0a3c85fe3e5d9293c2f88560be4a6165b71f94c
Steveplays28 <djspaargaren@outlook.com>
2024-02-09 16:30:54 +0100

    refactor: Reimplement Minecraft's `Random` interface with splittable random generator

--- src/main/java/com/github/anopensaucedev/fasterrandom/FasterRandom.java
+++ src/main/java/com/github/anopensaucedev/fasterrandom/FasterRandom.java
@@ -4,10 +4,15 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.random.RandomGenerator;
+import java.util.random.RandomGeneratorFactory;
+
 public class FasterRandom implements ModInitializer {
 	public static final String MOD_ID = "faster-random";
 	public static final String MOD_NAME = "Faster Random";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+	public static final RandomGeneratorFactory<RandomGenerator.SplittableGenerator> RANDOM_GENERATOR_FACTORY = RandomGeneratorFactory.of(
+			"L128X128MixRandom");
 
 	@Override
 	public void onInitialize() {
