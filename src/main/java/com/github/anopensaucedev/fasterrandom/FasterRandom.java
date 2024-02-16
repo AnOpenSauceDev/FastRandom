d0b2a5e8c28fed7eb3c9b8359ab120dbc2ef76b3
AnOpenSauceDev <noreply@anopensauce.dev>
2024-02-16 19:34:05 +1000

    This benchmark produces 100X slower/faster results?
    No idea why ThreadLocalRandom is 100X faster here, when it _looks_ normal, and it isn't nearly as slow in `RandBench.java`.
    
    @Steveplays28

--- src/main/java/com/github/anopensaucedev/fasterrandom/FasterRandom.java
+++ src/main/java/com/github/anopensaucedev/fasterrandom/FasterRandom.java
@@ -2,9 +2,12 @@ package com.github.anopensaucedev.fasterrandom;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.util.math.random.RandomSeed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.random.RandomGenerator;
 import java.util.random.RandomGeneratorFactory;
 
@@ -26,9 +29,47 @@ public class FasterRandom implements ModInitializer {
 		LOGGER.warn("If you have any terrain-related issues, make sure to mention me (AnOpenSauceDev) in an issue just to be sure!");
 		LOGGER.info("Faster Random Issue Tracker: https://github.com/AnOpenSauceDev/FastRandom/issues");
 
+
 		//run random benchmarks
 		if(FabricLoader.getInstance().isDevelopmentEnvironment()){
+			LegacyBenchmark(); // older benchmark from Methane, where CheckedRandom is 100x slower than Threadlocal?
 			RandBench.StartBenchmark();
+		}
+
+	}
+
+	// I have absolutely no idea why it's slower in this case.
+	public static void LegacyBenchmark(){
+
+		int iterations = 1000000000;
+
+		ThreadLocalRandom random = ThreadLocalRandom.current();
+
+		long time = System.nanoTime();
+
+		for(int x = 0; x < iterations; x++ ){
+			random.nextFloat();
+		}
+
+		long finaltime = System.nanoTime() - time;
+
+		LOGGER.info("Random Benchmark Results (ThreadLocal): " + RandBench.toSeconds(finaltime));
+
+		Random random2 = Random.create(RandomSeed.getSeed()); //seeded or not, this is always slower
+
+		long time2 = System.nanoTime();
+
+		for(int x = 0; x < iterations; x++ ){
+			random2.nextFloat();
+		}
+
+
+		long finaltime2 = System.nanoTime() - time2;
+
+		LOGGER.info("Random Benchmark Results (Random): " + RandBench.toSeconds(finaltime2));
+
+		if(finaltime < finaltime2){
+			LOGGER.info("ThreadLocal was faster!");
 		}
 
 	}
