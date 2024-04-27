d58d51743eeaa7f4c8637588b969e189f3f65f44
AnOpenSauceDev <119765865+AnOpenSauceDev@users.noreply.github.com>
2024-04-27 18:57:09 +1000

    4.0.1

--- src/main/java/com/github/anopensaucedev/fasterrandom/FasterRandomPreLaunch.java
+++ src/main/java/com/github/anopensaucedev/fasterrandom/FasterRandomPreLaunch.java
@@ -15,7 +15,7 @@ public class FasterRandomPreLaunch implements PreLaunchEntrypoint {
 		LOGGER.info("Preparing to launch Faster Random!");
 		LOGGER.info("If Minecraft crashes past this point, your JVM is not supported.");
 		try{
-			Class.forName("java.util.random.RandomGeneratorFactory");
+			Class.forName("java.util.random.RandomGeneratorFactory"); // check for JVM support.
 		}catch (ClassNotFoundException exception){
 			LOGGER.error("Your JVM does not support Faster Random! Crashing game!");
 			JOptionPane.showMessageDialog(null,"._. Faster Random has crashed... \nERROR: Your Java Virtual Machine (JVM) does not support the features that Faster Random absolutely requires to run. \nPlease get a supported JVM such as GraalVM (at https://www.graalvm.org/) or Azul JDK (at https://www.azul.com/downloads/#zulu). \nYour JVM, " + System.getProperty("java.vm.name") + " " + System.getProperty("java.vm.version")  + " Is either too old, or is not suitable for Faster Random. \nMinecraft will now crash.", "Faster Random Loader",JOptionPane.WARNING_MESSAGE);
